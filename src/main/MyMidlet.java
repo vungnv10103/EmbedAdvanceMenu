08029545a6a4c4a93e4349792d9600be9c632369
Vung Nguyen <vungnguyenn1001@gmail.com>
2024-09-26 23:52:24 +0700

    initial commit

---
+++ src/main/MyMidlet.java
@@ -0,0 +1,30 @@
+package main;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class MyMidlet extends MIDlet implements Runnable, CommandListener {
+
+	public void commandAction(Command cmd, Displayable displayable) {
+
+	}
+
+	public void run() {
+
+	}
+
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+
+	}
+
+	protected void pauseApp() {
+
+	}
+
+	protected void startApp() throws MIDletStateChangeException {
+
+	}
+}
